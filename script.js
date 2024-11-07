4279a364711f20f453b49092576734e134f9cda3
shenayata123 <shenayata23@gmail.com>
2024-11-06 21:59:09 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('changeColorButton');
+    
+    button.addEventListener('click', () => {
+        document.body.style.backgroundColor = getRandomColor();
+    });
+    
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+});
+
